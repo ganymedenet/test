14767bf6b19480fe8d9eb243698fa70f8244536f
ganynmednet <Bsdjh381%>
2019-09-25 15:10:31 +0100

    ыв

--- test.py
+++ test.py
@@ -1,3 +1,14 @@
-print('hello world')
+import time
 
-# it works OFFICE _2
+start_time = time.time()
+
+i = 1
+while i < 990000000:
+  c = 999*999
+  i += 1
+
+elapsed_time = time.time() - start_time
+
+print(elapsed_time)
+
+
