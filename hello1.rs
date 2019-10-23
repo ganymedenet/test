745ee69473307bb422280c910c079ca17226e908
ganynmednet <51125243+ganynmednet@users.noreply.github.com>
2019-10-23 18:54:11 +0100

    Update hello1.rs

--- hello1.rs
+++ hello1.rs
@@ -7,7 +7,7 @@ let now = Instant::now();
 let mut n = 1; 
 let mut x:i128 = 1;
 
-while n < 999999 {
+while n < 9999999 {
 
 x = 999 * 999;
 
@@ -15,6 +15,6 @@ n += 1;
 } 
   
 println!("The value of x is: {}\n", x);
-println!("{}", now.elapsed().as_nanos());
+println!("{}", now.elapsed().as_secs());
 
 }
