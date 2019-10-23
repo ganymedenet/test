2e0e6ddd1602b6cf319129dd9eaf7d2be7b07f2d
ganynmednet <51125243+ganynmednet@users.noreply.github.com>
2019-10-23 18:50:35 +0100

    Create hello1.rs

---
+++ hello1.rs
@@ -0,0 +1,20 @@
+use std::time::{Instant};
+
+fn main(){
+ 
+let now = Instant::now();
+
+let mut n = 1; 
+let mut x:i128 = 1;
+
+while n < 999 {
+
+x = 999 * 999;
+
+n += 1;
+} 
+  
+println!("The value of x is: {}\n", x);
+println!("{}", now.elapsed().as_secs());
+
+}
